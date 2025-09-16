a32d70ed811d925a473669598729c70e1940a0df
nafe333 <94410012+nafe333@users.noreply.github.com>
2025-09-16 19:00:27 +0300

    added tab bar and favorites view controller and home view controller and added animated launch screen

---
+++ SportApp/SportsCollectionViewCell/SportsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SportsCollectionViewCell.swift
+//  SportApp
+//
+//  Created by Nafea Elkassas on 16/09/2025.
+//
+
+import UIKit
+
+class SportsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
