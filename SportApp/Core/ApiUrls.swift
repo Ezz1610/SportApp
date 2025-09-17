388dc37d52b4fc02178ea76a3479b014dedb336a
Ezz1610 <ezz019960@gmail.com>
2025-09-17 04:59:49 -0700

    add core folder and ApiURls class

---
+++ SportApp/Core/ApiUrls.swift
@@ -0,0 +1,18 @@
+//
+//  ApiUrls.swift
+//  SportApp
+//
+//  Created by mohamed ezz on 17/09/2025.
+//
+
+import Foundation
+var apiKey : String = "2120f2f312d80b0c590180019e76e23dd650d20e9d0df7f0e778e0d0e6afc9bd"
+import Foundation
+class ApiUrls{
+    static let baseUrl = "https://apiv2.allsportsapi.com"
+    static let football  = "\(baseUrl)/football/?met=Countries&APIkey=\(apiKey)"
+    static let basketball  = "\(baseUrl)/basketball/?met=Countries&APIkey=\(apiKey)"
+    static let cricket  = "\(baseUrl)/cricket/?met=Countries&APIkey=\(apiKey)"
+    static let tennis  = "\(baseUrl)/tennis/?met=Countries&APIkey=\(apiKey)"
+    
+}
