f3a981bbb957bb298cd9612f8acc391f48ba9233
Ezz1610 <ezz019960@gmail.com>
2025-09-20 13:07:45 -0700

    delete teams folder and refactor end points of fixtures

---
+++ SportApp/Core/Constants/AppConstants.swift
@@ -0,0 +1,46 @@
+//
+//  AppConstants.swift
+//  SportApp
+//
+//  Created by mohamed ezz on 20/09/2025.
+//
+
+import Foundation
+
+class AppConstants {
+    
+    private static let calendar = Calendar.current
+
+    // MARK: - Public Methods
+
+    /// Returns the current date
+    static func getNowDate() -> Date {
+        return Date()
+    }
+
+    /// Returns the first day of the current month
+    static func getFirstDateOfCurrentMonth() -> Date? {
+        let now = Date()
+        let components = calendar.dateComponents([.year, .month], from: now)
+        return calendar.date(from: components) ?? now
+    }
+
+    /// Returns the last day of the current month
+    static func getLastDateOfCurrentMonth() -> Date? {
+        guard let firstDay = getFirstDateOfCurrentMonth() else { return nil }
+        
+        var offset = DateComponents()
+        offset.month = 1
+        offset.day = -1
+        
+        return calendar.date(byAdding: offset, to: firstDay) ?? firstDay
+    }
+
+    /// Helper: Formats a Date into "yy-MM-dd"
+    static func formatDate(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yy-MM-dd"
+        formatter.timeZone = TimeZone.current
+        return formatter.string(from: date)
+    }
+}
