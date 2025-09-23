427f9c6a657813b4cc838ccadfdf413f4b0da850
nafe333 <94410012+nafe333@users.noreply.github.com>
2025-09-23 15:59:07 +0300

    updated tennis league request

--- SportApp/Features/Leagues/Model/TennisLeagueRequest.swift
+++ SportApp/Features/Leagues/Model/TennisLeagueRequest.swift
@@ -9,8 +9,8 @@ import Foundation
 struct TennisLeagueRequest : Decodable{
     var result : [TennisLeague]
     var success : Int
-
 }
+
 struct TennisLeague : Decodable{
     var league_key : Int
     var league_name : String
